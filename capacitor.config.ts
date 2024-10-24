137b6e9066b78c73406f45afad55a000a2ecb253
Kr-luis <kluis816@gmail.com>
2024-10-23 22:54:51 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quitotech',
+  webDir: 'www'
+};
+
+export default config;
